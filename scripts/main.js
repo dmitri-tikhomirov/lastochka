546c3654a4b4b55b40d69b0f37e7e0c194320b51
Dmitri Tikhomirov <dmitri.tikhomirov@gmail.com>
2021-07-25 09:25:09 +0300

    Price tag, about us, cards, nav enhancements, optimizations: fonts, transitions

--- scripts/main.js
+++ scripts/main.js
@@ -1,68 +1,140 @@
-// Priority navigation
-if (window.matchMedia('(min-width: 481px)').matches) {
-  // Remove CSS fallback used if no JavaScript is available
-  document.querySelector('nav').classList.remove('nav-no-javascript');
+/*******************************************************************************
+* Navigation *
+*******************************************************************************/
 
-  // Hide nav and dropdown items
-  const navItems = document.querySelectorAll('nav > a:not(.nav-mobile)');
-  const navMoreAndDropdown = document.querySelector('.nav-more-and-dropdown');
-  const navDropdownHover = document.querySelector('.nav-dropdown-hover');
-  const navDropdownIcon = document.querySelector('.nav-dropdown-icon');
-  const navDropdownItems = document.querySelectorAll('.nav-dropdown > a');
+const navMoreAndDropdown = document.querySelector('.nav-more-and-dropdown');
+const navDropdownHoverable = document.querySelector('.nav-dropdown-hoverable');
+const navMore = document.querySelector('.nav-more');
+const navMoreIcon = document.querySelector('.nav-more-icon');
+const navMobileIcon = document.querySelector('.nav-mobile-icon');
+const navCloseIcon = document.querySelector('.nav-close-icon');
+let dropdownOpened = false;
+let dropdownJustOpenedByMouseEnter = false;
 
-  const navAdapt = () => {
-    // Reveal all items before calculation
-    navItems.forEach((item) => {
-      item.classList.remove('hide');
-    });
+function openDropdown() {
+  navDropdownHoverable.style.opacity = '1';
+  navDropdownHoverable.style.visibility = 'visible';
+  navMore.style.opacity =
+    window.getComputedStyle(navMore).getPropertyValue('--opacity-l');
+  navMoreIcon.style.transform = 'rotate(180deg)';
+  navMobileIcon.style.opacity = '0';
+  navCloseIcon.style.opacity =
+    window.getComputedStyle(navCloseIcon).getPropertyValue('--opacity-l');
+  dropdownOpened = true;
+}
 
-    navDropdownItems.forEach((item) => {
-      item.classList.remove('hide');
-    });
+function closeDropdown() {
+  navDropdownHoverable.style.opacity = '';
+  navDropdownHoverable.style.visibility = '';
+  navMore.style.opacity = '';
+  navMoreIcon.style.transform = '';
+  navMobileIcon.style.opacity = '';
+  navCloseIcon.style.opacity = '';
+  dropdownOpened = false;
+}
 
-    navMoreAndDropdown.style.display = 'block';
+navMoreAndDropdown.addEventListener('mouseenter', (event) => {
+  openDropdown();
+  dropdownJustOpenedByMouseEnter = true;
+  setTimeout(() => {
+    dropdownJustOpenedByMouseEnter = false;
+  }, 1);
+});
 
-    // Hide nav items that don't fit
-    const navWidth = document.querySelector('nav').offsetWidth;
-    let navMoreWidth = navMoreAndDropdown.offsetWidth +
-                      parseFloat(window.getComputedStyle(navMoreAndDropdown)
-                                        .getPropertyValue('margin-left'));
-    let stopWidth = navMoreWidth;
-    let navHiddenItems = [];
-    let stopReached = false;
+navMoreAndDropdown.addEventListener('mouseleave', (event) => {
+  if (dropdownOpened) {
+    closeDropdown();
+  }
+});
 
-    navItems.forEach((item, index, array) => {
-      if (stopReached === false) {
-        let itemMargin = parseFloat(window.getComputedStyle(item)
-                                          .getPropertyValue('margin-left'));
-        // Try to fit the last item without "more"
-        if (index === array.length - 1) {
-          stopWidth -= navMoreWidth;
-        }
-        if (navWidth >= stopWidth + item.offsetWidth + itemMargin) {
-          stopWidth = stopWidth + item.offsetWidth + itemMargin;
-        } else {
-          stopReached = true;
-        }
-      }
-      if (stopReached === true) {
-        item.classList.add('hide');
-        navHiddenItems.push(index);
-      }
-    });
-
-    // Hide "more" and dropdown items
-    if (!navHiddenItems.length) {
-      navMoreAndDropdown.style.display = '';
+document.addEventListener('click', (event) => {
+  if (dropdownJustOpenedByMouseEnter) {
+    return;
+  }
+  // Add hover effects on tap
+  if (!dropdownOpened && navMoreAndDropdown.contains(event.target)) {
+    openDropdown();
+    return;
+  }
+  // Remove hover effects on outside tap
+  if (dropdownOpened) {
+    if (!navDropdownHoverable.contains(event.target)) {
+      closeDropdown();
+      return;
     } else {
-      navDropdownItems.forEach((item, index) => {
-        if(!navHiddenItems.includes(index)) {
-          item.classList.add('hide');
+      // Remove hover effects on tapping or clicking a link
+      const navDropdownLinks = document.querySelectorAll('.nav-dropdown a');
+      navDropdownLinks.forEach((element) => {
+        if (element.contains(event.target)) {
+          closeDropdown();
+          return;
         }
       });
     }
   }
+});
 
-  navAdapt(); // Adapt on load
-  window.addEventListener('resize', navAdapt); // Adapt on window resize
+/*******************************************************************************
+* Priority navigation *
+*******************************************************************************/
+
+// Remove CSS fallback used if no JavaScript is available
+document.querySelector('.nav').classList.remove('nav-no-javascript');
+
+// Hide nav and dropdown items
+const navItems = document.querySelectorAll('.nav-item');
+const navDropdownItems = document.querySelectorAll('.nav-item-dropdown');
+
+const navAdapt = () => {
+  // Reveal all items before calculation
+  navItems.forEach((item) => {
+    item.classList.remove('hide');
+  });
+
+  navDropdownItems.forEach((item) => {
+    item.classList.remove('hide');
+  });
+
+  navMoreAndDropdown.style.display = 'block';
+
+  // Hide nav items that don't fit
+  const navWidth = document.querySelector('.nav').offsetWidth;
+  let navMoreWidth = navMoreAndDropdown.offsetWidth;
+  let stopWidth = navMoreWidth;
+  let navHiddenItems = [];
+  let stopReached = false;
+
+  navItems.forEach((item, index, array) => {
+    if (stopReached === false) {
+      // Try to fit the last item without "more"
+      if (index === array.length - 1) {
+        stopWidth -= navMoreWidth;
+      }
+      if (navWidth >= stopWidth + item.offsetWidth) {
+        stopWidth = stopWidth + item.offsetWidth;
+      } else {
+        stopReached = true;
+      }
+    }
+    if (stopReached === true) {
+      item.classList.add('hide');
+      navHiddenItems.push(index);
+    }
+  });
+
+  // Hide "more" and dropdown items
+  if (!navHiddenItems.length) {
+    navMoreAndDropdown.style.display = '';
+  } else {
+    navDropdownItems.forEach((item, index) => {
+      if(!navHiddenItems.includes(index)) {
+        item.classList.add('hide');
+      }
+    });
+  }
 }
+
+// Adapt immediately
+navAdapt();
+// Adapt on window resize
+window.addEventListener('resize', navAdapt);
